7dff8c17e19291a227f3c8a118187e7e74798006
Q <qualuo@gmail.com>
2019-07-17 05:57:55 +0200

    Change to new darker theme design

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top + 10)
+          scrollTop: (target.offset().top + 30)
         }, 1000, "easeInOutExpo");
         return false;
       }
