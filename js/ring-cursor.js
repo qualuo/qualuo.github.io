a3956eff00346aafcccd25187510b988e033705a
Quang Luong <qualuo@gmail.com>
2023-01-23 08:43:31 +0100

    Hide initial ring cursor

--- js/ring-cursor.js
+++ js/ring-cursor.js
@@ -1,7 +1,9 @@
 const cursor = document.querySelector('.ring-cursor');
+cursor.style.visibility = 'hidden';
 let ringDiameter = 50;
 
 document.addEventListener('mousemove', (e) => {
+    cursor.style.visibility = 'visible';
     cursor.style.left = e.pageX - ringDiameter/2 + 'px';
     cursor.style.top = e.pageY - ringDiameter/2 + 'px';
 });
